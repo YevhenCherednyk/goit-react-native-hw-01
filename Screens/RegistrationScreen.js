05151b3373f88f60c3923201d210c0c616601e81
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2023-02-18 17:33:22 +0200

    add. registration page

---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,226 @@
+import react, { useState } from "react";
+import {
+  StyleSheet,
+  ImageBackground,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
+export const RegistrationScreen = () => {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  const onSubmit = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  const changeInput = (name, value) => {
+    setState((prevState) => ({ ...prevState, name: value }));
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.img}
+          source={require("../assets/images/BG.jpg")}
+        >
+          <KeyboardAvoidingView
+            style={styles.wrapper}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.registerWrapper,
+                marginBottom: isShowKeyboard ? -140 : 0,
+              }}
+            >
+              <View
+                style={[
+                  styles.userImg,
+                  {
+                    transform: [{ translateX: -60 }],
+                  },
+                ]}
+              ></View>
+              <Text style={styles.registerTittle}>Регистрация</Text>
+              <View style={styles.form}>
+                <TextInput
+                  style={{
+                    ...styles.input,
+                    marginBottom: 16,
+                    backgroundColor: state.login ? "#fff" : "#f6f6f6",
+                    borderColor: state.login ? "#ff6c00" : "#e8e8e8",
+                  }}
+                  placeholder="Логин"
+                  placeholderTextColor="#bdbdbd"
+                  value={state.login}
+                  onFocus={() => {
+                    setIsShowKeyboard(true);
+                  }}
+                  onChangeText={(value) => {
+                    setState((prevState) => ({ ...prevState, login: value }));
+                  }}
+                ></TextInput>
+                <TextInput
+                  style={{
+                    ...styles.input,
+                    marginBottom: 16,
+                    backgroundColor: state.email ? "#fff" : "#f6f6f6",
+                    borderColor: state.email ? "#ff6c00" : "#e8e8e8",
+                  }}
+                  placeholder="Адрес электронной почты"
+                  placeholderTextColor="#bdbdbd"
+                  value={state.email}
+                  onFocus={() => {
+                    setIsShowKeyboard(true);
+                  }}
+                  onChangeText={(value) => {
+                    setState((prevState) => ({
+                      ...prevState,
+                      email: value,
+                    }));
+                  }}
+                ></TextInput>
+                <TextInput
+                  style={{
+                    ...styles.input,
+                    marginBottom: 43,
+                    backgroundColor: state.password ? "#fff" : "#f6f6f6",
+                    borderColor: state.password ? "#ff6c00" : "#e8e8e8",
+                  }}
+                  placeholder="Пароль"
+                  placeholderTextColor="#bdbdbd"
+                  secureTextEntry={true}
+                  value={state.password}
+                  onFocus={() => {
+                    setIsShowKeyboard(true);
+                  }}
+                  onChangeText={(value) => {
+                    setState((prevState) => ({
+                      ...prevState,
+                      password: value,
+                    }));
+                  }}
+                ></TextInput>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  style={styles.button}
+                  onPress={onSubmit}
+                >
+                  <Text style={styles.btnTxt}>Зарегистрироваться</Text>
+                </TouchableOpacity>
+                <Text style={styles.registerTxt}>Уже есть аккаунт? Войти</Text>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+
+  img: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+
+  wrapper: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+
+  registerWrapper: {
+    position: "relative",
+    paddingTop: 92,
+    paddingBottom: 45,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#fff",
+  },
+
+  userImg: {
+    position: "absolute",
+    top: -60,
+    left: "50%",
+    width: 120,
+    height: 120,
+    backgroundColor: "#f6f6f6",
+    borderRadius: 16,
+  },
+
+  registerTittle: {
+    paddingBottom: 32,
+    // fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+    color: "#212121",
+  },
+
+  form: { paddingHorizontal: 16 },
+
+  input: {
+    paddingHorizontal: 16,
+    fontSize: 16,
+    lineHeight: 19,
+    borderWidth: 1,
+    height: 50,
+    borderRadius: 8,
+    borderWidth: 1,
+    color: "#212121",
+  },
+
+  text: {
+    color: "#000",
+    fontSize: 18,
+  },
+
+  button: {
+    height: 51,
+    backgroundColor: "#ff6c00",
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 16,
+  },
+
+  btnTxt: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#fff",
+  },
+
+  registerTxt: {
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#1b4371",
+  },
+});
